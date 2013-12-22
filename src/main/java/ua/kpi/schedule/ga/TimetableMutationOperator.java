f373ed3843042721a95be18693220b30e99a4304
Nastyshka <Nastya.rudyk@gmail.com>
2013-12-22 22:00:24 +0200

    added genetic algorithm http://jgap.sourceforge.net/doc/#documentation/// https://code.google.com/p/univtimetable/source/browse/Timetable2/core/src/ua/kiev/univ/

---
+++ src/main/java/ua/kpi/schedule/ga/TimetableMutationOperator.java
@@ -0,0 +1,35 @@
+package ua.kpi.schedule.ga;
+
+import org.jgap.Configuration;
+import org.jgap.IUniversalRateCalculator;
+import org.jgap.InvalidConfigurationException;
+import org.jgap.Population;
+import org.jgap.impl.MutationOperator;
+
+import java.util.List;
+
+public class TimetableMutationOperator extends MutationOperator {
+    public TimetableMutationOperator(Configuration configuration,
+                                     int i) throws InvalidConfigurationException {
+        super(configuration, i);
+    }
+
+    public TimetableMutationOperator(Configuration configuration,
+                                     IUniversalRateCalculator iUniversalRateCalculator) throws InvalidConfigurationException {
+        super(configuration, iUniversalRateCalculator);
+    }
+
+    public TimetableMutationOperator(Configuration configuration) throws InvalidConfigurationException {
+        super(configuration);
+    }
+
+    public TimetableMutationOperator() throws InvalidConfigurationException {
+        super();
+    }
+
+    @Override
+    public void operate(Population a_population, List a_candidateChromosomes) {
+        super.operate(a_population, a_candidateChromosomes);
+        //System.out.println("TimetableMutationOperator invoked!");
+    }
+}
