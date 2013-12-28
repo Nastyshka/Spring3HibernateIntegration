07e1cf314e284eb3e1f7ece2f9fbed916106499a
AnastasiyaR <Anastasiya.Rudyk@miratechgroop.com>
2013-12-28 11:58:27 +0200

    constants

--- src/main/java/ua/kpi/schedule/ga/Start.java
+++ src/main/java/ua/kpi/schedule/ga/Start.java
@@ -17,9 +17,6 @@ public class Start {
     private static final String GENOTYPE_FILENAME = "D:\\population.xml";
     private static final String BEST_CHROMOSOME_FILENAME = "D:\\best_chromosome.xml";
     private static final String XML_TEST_FILENAME = "D:\\inputTimetable.xml";
-//    protected static int POPULATION_SIZE = 30;
-//    public static double THRESHOLD;
-//    public static Integer CHROMOSOME_SIZE = 5;
     private static long start_t = 0;
     private static long finish_t = 0;
 
